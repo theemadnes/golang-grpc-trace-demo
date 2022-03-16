2316afcce711153c2ae261649396c784ad1ab77f
theemadnes <alex.mattson@gmail.com>
2022-03-16 12:59:53 -0500

    backend started

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
 
-	pb "github.com/theemadnes/golang-grpc-trace-demo"
+	pb "github.com/theemadnes/golang-grpc-trace-demo/pingpong"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -12,4 +18,24 @@ var (
 
 type server struct {
 	pb.UnimplementedPingPongServer
+}
+
+func (s *server) PongReply(ctx context.Context, in *pb.Ping) (*pb.Pong, error) {
+	log.Printf("Received ping: %v", in.Ping)
+	currentTime := time.Now()
+	return &pb.Pong{Pong: "Pong @ " + currentTime.String()}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPingPongServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
